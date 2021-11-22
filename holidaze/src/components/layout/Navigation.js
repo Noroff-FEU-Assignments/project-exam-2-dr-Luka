f65022242e5399418aeb384a3d19bd3a5d65bc63
dr-Luka <luka@learnx.no>
2021-11-22 22:51:54 +0100

    Update Navigation.js
    Navigation up and running, I need to add links

--- holidaze/src/components/layout/Navigation.js
+++ holidaze/src/components/layout/Navigation.js
@@ -6,14 +6,21 @@ import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg">
       <Container>
-        <Navbar.Brand href="/">Holidaze</Navbar.Brand>
-        <Nav className="me-auto">
-          <Link to="/">Home</Link>
-          <Link to="/Contact">Contact</Link>
-          <Link to="/Login">Login</Link>
-        </Nav>
+        <Navbar.Brand href="#home">Holidaze</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+            <Link to="/Login">
+              <Nav.Link eventKey={2}>Logindadw</Nav.Link>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
