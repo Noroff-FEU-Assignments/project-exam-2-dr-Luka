ceedce26b37883c4f67c72cec59bf5b33bfe0ebd
dr-Luka <luka@learnx.no>
2021-11-22 23:16:13 +0100

    NavBar Links working

--- holidaze/src/components/layout/Navigation.js
+++ holidaze/src/components/layout/Navigation.js
@@ -2,23 +2,21 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 
-import { Link } from "react-router-dom";
-
 export default function Navigation() {
   return (
     <Navbar collapseOnSelect expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Holidaze</Navbar.Brand>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/Contact">Contact</Nav.Link>
           </Nav>
           <Nav>
-            <Link to="/Login">
-              <Nav.Link eventKey={2}>Logindadw</Nav.Link>
-            </Link>
+            <Nav.Link eventKey={2} href="/Login">
+              Login
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
