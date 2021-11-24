6a15c0264a893553cdd40116cd6419c04bbb8407
dr-Luka <luka@learnx.no>
2021-11-24 23:09:55 +0100

    API Called and Working

---
+++ holidaze/src/components/home/EstablishmentsList.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { API } from "../../constants/api";
+
+import EstablishmentCard from "../layout/EstablishmentCard";
+
+export default function EstablishmentList() {
+  const [Establishments, setEstablishments] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const response = await fetch(API);
+
+        if (response.ok) {
+          const json = await response.json();
+
+          setEstablishments(json);
+        } else {
+          setError("An error occured");
+        }
+      } catch (error) {
+        setError(error.toString());
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>ERROR: An error occured</div>;
+  }
+
+  return (
+    <div className="Establishments">
+      {Establishments.map(function (Establishment) {
+        const { id, name, description } = Establishment;
+        return (
+          <EstablishmentCard
+            key={id}
+            id={id}
+            name={name}
+            description={description}
+          />
+        );
+      })}
+    </div>
+  );
+}
--- holidaze/src/components/home/Home.js
+++ holidaze/src/components/home/Home.js
@@ -1,8 +1,10 @@
 import Heading from "../layout/Heading";
+import EstablishmentList from "./EstablishmentsList";
 export default function Home() {
   return (
     <>
-      <Heading content="Home" size="1" className="home" />
+      <Heading content="Home" size="1" />
+      <EstablishmentList />
     </>
   );
 }
