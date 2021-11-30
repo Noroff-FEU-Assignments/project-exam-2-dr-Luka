bdf5abfc746783d03ed80fb260de3077fae94c1f
dr-Luka <luka@learnx.no>
2021-11-30 21:13:04 +0100

    Contact form w/out Styling

--- holidaze/src/components/contact/Contact.js
+++ holidaze/src/components/contact/Contact.js
@@ -1,8 +1,11 @@
 import Heading from "../layout/Heading";
+import ContactForm from "./ContactForm";
+
 export default function Contact() {
   return (
     <>
       <Heading content="Contact" size="2" />
+      <ContactForm />
     </>
   );
 }
---
+++ holidaze/src/components/contact/ContactForm.js
@@ -0,0 +1,44 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required("Please enter your name")
+    .min(1, "Name must be at least 1 characters"),
+  email: yup
+    .string()
+    .required("Please enter an email address")
+    .email("Please enter a valid email address"),
+  message: yup
+    .string()
+    .required("Please enter your message")
+    .min(10, "The message must be at least 10 characters"),
+});
+
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  function onSubmit(data) {}
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("name")} placeholder="Name" />
+      {errors.name && <span>{errors.name.message}</span>}
+
+      <input {...register("email")} placeholder="Email" />
+      {errors.email && <span>{errors.email.message}</span>}
+
+      <textarea {...register("message")} placeholder="Message" />
+      {errors.message && <span>{errors.message.message}</span>}
+
+      <button>Send</button>
+    </form>
+  );
+}
