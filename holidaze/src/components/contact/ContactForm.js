60630d218096764c2c4afc9c26fae12398287376
dr-Luka <luka@learnx.no>
2021-11-30 22:23:36 +0100

    Form Styled/ Axios installed

--- holidaze/src/components/contact/ContactForm.js
+++ holidaze/src/components/contact/ContactForm.js
@@ -5,16 +5,16 @@ import { yupResolver } from "@hookform/resolvers/yup";
 const schema = yup.object().shape({
   name: yup
     .string()
-    .required("Please enter your name")
-    .min(1, "Name must be at least 1 characters"),
+    .required("Please enter your name!")
+    .min(1, "Name must be at least 1 characters!"),
   email: yup
     .string()
-    .required("Please enter an email address")
-    .email("Please enter a valid email address"),
+    .required("Please enter an email address!")
+    .email("Please enter a valid email address!"),
   message: yup
     .string()
-    .required("Please enter your message")
-    .min(10, "The message must be at least 10 characters"),
+    .required("Please enter your message!")
+    .min(10, "The message must be at least 10 characters!"),
 });
 
 export default function ContactForm() {
@@ -25,30 +25,41 @@ export default function ContactForm() {
   } = useForm({
     resolver: yupResolver(schema),
   });
-  function onSubmit(data) {}
+
+  function onSubmit(data) {
+    console.log(data);
+  }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("name")} placeholder="Name" className="form-info" />
-      {errors.name && <span class="form-error">{errors.name.message}</span>}
+    <form onSubmit={handleSubmit(onSubmit)} className="contactForm">
+      <input
+        {...register("name")}
+        placeholder="Name"
+        className="form-info block"
+      />
+      {errors.name && (
+        <span className="form-error block">{errors.name.message}</span>
+      )}
 
       <input
         {...register("email")}
         placeholder="Email"
-        className="form-email"
+        className="form-info block"
       />
-      {errors.email && <span class="form-info">{errors.email.message}</span>}
+      {errors.email && (
+        <span className="form-error block">{errors.email.message}</span>
+      )}
 
       <textarea
         {...register("message")}
         placeholder="Message"
-        className="form-message"
+        className="form-message block"
       />
       {errors.message && (
-        <span class="form-error">{errors.message.message}</span>
+        <span className="form-error block">{errors.message.message}</span>
       )}
 
-      <button>Send</button>
+      <button className="BtnConfirm">Send</button>
     </form>
   );
 }
